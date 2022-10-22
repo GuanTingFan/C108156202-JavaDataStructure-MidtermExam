ac37b5ff56b76101e194938f9a93dbb8b6ac05ce
GuanTing <2649897@gmail.com>
2022-10-22 14:24:12 +0800

    init commit

---
+++ C108156202_Miterm_Exam/src/c108156202_miterm_exam/C108156202_Miterm_Exam.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package c108156202_miterm_exam;
+
+/**
+ *
+ * @author wangh
+ */
+public class C108156202_Miterm_Exam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
